1904d65fe629c1dca09f027cea26ed6ac3565875
eileenrose <erabuenaflor@gmail.com>
2017-03-04 16:16:55 -0800

    removing placeholders

--- js_style/style.js
+++ js_style/style.js
@@ -1,7 +1,26 @@
-function setup(){
-  $("button").css("color", "black");
+function courseAttriChosen(){
+  var chosen = $("#cratr").val();
+  if (chosen == "Breadth Requirements"){
+    $("#ifBreadth").hide();
+  }
+  else if (chosen == "Fall Program for Freshman"){
+    $("#ifFall").hide();
+  }
+  else{
+    return;
+  }
 }
 
 
+function setup(){
+
+  courseAttriChosen();
+}
+
+window.onload = function(){
+    $("button").css("color", "black");
+  $("#ifFall").hide();
+  $("#ifBreadth").hide();
+}
 
 $(document).ready(setup);
