d7e948ce489d8faef1f434a6b44c29d89cb3ee54
eileenrose <erabuenaflor@gmail.com>
2017-03-08 22:28:12 -0800

    deled most of the front end ;-;

--- js_style/style.js
+++ js_style/style.js
@@ -27,7 +27,7 @@ function setup(){
 window.onload(){
   $("#ifFall").hide();
   $("#ifBreadth").hide();
-}*/
+}
 function checkAt(){
   var chosen = $("#cratr").val();
   if (chosen == 1){
@@ -47,19 +47,49 @@ function checkAt(){
 }
 
 
-
+*/
 function setup(){
   $("button").css("color", "black");
   $("#search").css("color", "white");
   $("#search").css("font-size", "24px");
-  $("#ifFall").hide();
-  $("#ifBreadth").hide();
-  $("#shortenHide").hide();
-  $("#cratr").mouseleave(checkAt)
-  $("#shorten").click(function(){
+  //$("#ifFall").hide();
+  //$("#ifBreadth").hide();
+  //$("#shortenHide").hide();
+  //$("#cratr").mouseleave(checkAt)
+  /*$("#shorten").click(function(){
     $("#shortenHide").show();
-  });
+  });*/
 
+}
+
+
+function validateClass(){
+  var validTerm = $("#semester option:selected").val();
+  var validSub = $("#subjects option:selected").val();
+  var validCareer = $("#career option:selected").val();
+  var validCRN = document.forms.userData.courseNum.value;
+  var validProceed = true;
+
+  if (validTerm == "notValid" ){
+    alert("You are missing Term");
+    validProceed = false;
+  }
+  if (validSub == "notValid" ){
+    alert("You are missing Subject");
+    validProceed = false;
+  }
+  if (validCRN == "" ){
+    alert("You are missing Course Number");
+    validProceed = false;
+  }
+  if (validCareer == "notValid" ){
+    alert("You are missing Career");
+    validProceed = false;
+  }
+
+  if (validProceed == true){
+    window.location.replace("notification.html");
+  }
 }
 
 $(document).ready(setup);
