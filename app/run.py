b7e7c089356972b4cf240f7e91503ba90047a5bf
eileenrose <erabuenaflor@gmail.com>
2017-04-03 19:09:05 -0700

    testing

--- app/run.py
+++ app/run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, jsonify
 import os
 from status import main
 
